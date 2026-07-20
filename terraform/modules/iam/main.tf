5bef55584b29cd645aec0d4a3bd7df83de43d0f1
Utkarsh Bishnoi <utkarshsysadmn@gmail.com>
2026-07-20 19:33:38 +0000

    style(terraform): format terraform code

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -16,7 +16,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 
 resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
 
-  role       = aws_iam_role.ec2_role.name
+  role = aws_iam_role.ec2_role.name
 
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
